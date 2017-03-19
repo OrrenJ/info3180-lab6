bc3b0d3af271c36ff008308b847ddee5c891f540
Orren Joseph <ojey11@gmail.com>
2017-03-19 11:37:08 -0500

    step 4

--- js/app.js
+++ js/app.js
@@ -5,11 +5,23 @@ app.controller("myCtrl", function($scope){
 	$scope.products = [ "Milk", "Bread", "Cheese" ];
 
 	$scope.addItem = function(){
-		$scope.products.push($scope.newItem);
-		$("#new_item").val("");
+
+		$scope.errortext = "";
+
+		if(!$scope.newItem){
+			return;
+		}
+
+		if($scope.products.indexOf($scope.newItem) == -1){
+			$scope.products.push($scope.newItem);
+			$("#new_item").val("");
+		} else {
+			$scope.errortext = "The item is already in your shopping list.";
+		}
 	}
 
 	$scope.removeItem = function(item){
+		$scope.errortext = "";
 		$scope.products.splice(item, 1);
 	}
 
